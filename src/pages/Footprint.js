df1a8a8a41263fd621846af806e09a14b69e81b5
Yunseop Kim <c.o.d.e@kakao.com>
2021-08-03 19:50:25 +0900

    발자취 페이지 추가

---
+++ src/pages/Footprint.js
@@ -0,0 +1,84 @@
+import { Link, Route, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import FadeIn from '../css/FadeIn'
+import Creatrip from "./Creatrip";
+import Mealant from "./Mealant";
+import NGensoft from "./NGensoft";
+
+const MainSection = styled.section`
+    max-width: 1024px;
+    ${FadeIn}
+`
+
+const Footprints = styled.ul`
+    position: relative;    
+    padding: 0;
+    list-style: none;
+    &:before {
+        display: block;
+        position: absolute;
+        left: -1.65rem;
+        content: '';
+        width: 8px;
+        height: 100%;
+        background: black;
+    }
+    & > li {
+        display: flex;
+        align-items: center;
+        margin: 2rem auto;
+        &:before {
+            position: absolute;
+            z-index: 1;
+            left: -2rem;
+            content: '';
+            width: 1rem;
+            height: 1rem;
+            border: 2px solid black;
+            border-radius: 50%;
+            background: white;
+            transition: background 0.5s;
+        }
+        &.selected:before {
+            background: green;
+        }
+    }
+`;
+
+const Article = styled.article`
+    margin: 2rem;
+    width: 40rem;
+    min-height: 30rem;
+`;
+
+const footprints = [
+    { path: '/footprint/ngensoft', title: '엔젠소프트', component: NGensoft },
+    { path: '/footprint/mealant', title: '밀랑', component: Mealant },
+    { path: '/footprint/creatrip', title: '크리에이트립', component: Creatrip },
+]
+
+const Footprint = () => {
+    const location = useLocation()
+    console.log(location)
+    return (<MainSection>
+        <div style={{ display: 'flex' }}>
+            <div>
+                <h1>발자취</h1>
+                <Footprints>
+                    {footprints.map(item => <li
+                        key={item.path}
+                        className={location.pathname === item.path ? 'selected' : ''}
+                    >
+                        <Link to={item.path}>{item.title}</Link>
+                    </li>)}
+                </Footprints>
+            </div>
+            <Article>
+                <Route path='/footprint' exact render={() => <img src="https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile7.uf.tistory.com%2Fimage%2F200F95274C39B4BE9E2019" />} />
+                {footprints.map(item => <Route key={item.path} path={item.path} component={item.component} />)}
+            </Article>
+        </div>
+    </MainSection>)
+}
+
+export default Footprint
---
+++ src/pages/Mealant.js
@@ -0,0 +1,34 @@
+import Ruby from '../components/Ruby'
+const Mealant = () => {
+    return <>
+        <h2>Mealant</h2>
+        <p>
+            <Ruby
+                text='밀랑에 와서 소프트웨어 개발 전반에 걸친 경험을 했어요. DB 설계부터 시작해서 API 개발 및 관리자 페이지까지 다양한 경험들을 했습니다.'
+                translation='When I came to Mealant, I had experience throughout software development. From DB design to API development and admin page, I have had various experiences.'
+            />
+        </p>
+        <p>
+            <Ruby
+                text='이곳에서도 좋은 동료들을 만나서 스타트업에서 일한다는 것의 기쁨과 열정을 느낄 수 있었던 시간이었어요. 제품에 대해 깊이 고민하고, 빠르게 제품을 출시해서 시장의 피드백을 받아 지속적으로 발전해가는, 린(Lean) 스타트업에 어떤 것인지에 대해 경험해볼 수 있었습니다.'
+                translation='It was also a time where I could feel the joy and passion of working for a startup by meeting good colleagues here. I was able to experience what it is like to be a Lean Startup that continuously develops by thinking deeply about a product, launching a product quickly, and receiving feedback from the market.'
+            />
+        </p>
+        <p>
+            <Ruby
+                text='감사하게도 이곳에서 파일럿 프로젝트로 시작했던 음식점 대기관리 앱이 시장에서 좋은 반응을 얻어서, 회사 메인 비즈니스로 성장해서 피버팅까지 하게 된 경험은 제게 아주 영광스러운 경험이었습니다.'
+                translation="Thankfully, the restaurant waiting management app that started as a pilot project here got a good response in the market, and it was a very honorable experience for me to grow into the company's main business and even pivot."
+            />
+        </p>
+        <h3>이런 것들을 만들었어요</h3>
+        <ul>
+            <li>테이블링 매장용 API 개발</li>
+            <li>테이블링 파트너센터 프론트엔드 개발</li>
+            <li>테이블링 고객용 API, 프론트엔드 개발 (현재 서비스 종료 및 모바일 앱 전환)</li>
+            <li>알림톡 및 문자 전송 서비스 개발</li>
+            <li>테이블링 대기관리 앱 최초 제안 및 개발</li>
+        </ul>
+    </>
+}
+
+export default Mealant
