336f3f6293cebe37e90dd697d2ca0c466399952e
Yunseop Kim <c.o.d.e@kakao.com>
2021-08-03 19:49:09 +0900

    인사 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Greetings from "./pages/Greetings";
+import About from "./pages/About";
+import { Route, useHistory, useLocation } from "react-router-dom";
+import Footprint from "./pages/Footprint";
+import Hobby from "./pages/Hobby";
 
-function App() {
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`
+
+const FixedBottomRight = styled.section`
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+`
+
+const Button = styled.button`
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: 1px solid;
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    background: #efefef;
+  }
+`
+
+const pages = [
+  { path: '/', component: Greetings, exact: true },
+  { path: '/about', component: About },
+  { path: '/footprint', component: Footprint },
+  { path: '/hobby', component: Hobby },
+]
+
+const App = () => {
+  const history = useHistory();
+  const location = useLocation();
+  function movePage(to) {
+    const current = pages.findIndex(page => page.path === location.pathname)
+    const path = pages?.[current + to]?.path
+    if (path) history.push(path)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {pages.map(page => <Route key={page.path} path={page.path} component={page.component} exact={page.exact} />)}
+      <FixedBottomRight>
+        <Button onClick={() => movePage(-1)}>◀️</Button>
+        <Button onClick={() => movePage(1)}>▶️</Button>
+      </FixedBottomRight>
+    </Wrapper>
   );
 }
 
---
+++ src/pages/Greetings.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import me from '../assets/IMG_2379.JPG';
+import Ruby from "../components/Ruby";
+import FadeIn from '../css/FadeIn'
+const MainSection = styled.section`
+  text-align: center;
+  opacity: 1;
+  ${FadeIn}
+`
+
+const SlicedImage = styled.img`
+  width: 12rem;
+  height: 24rem;
+  object-fit: cover;
+  margin: 1rem;
+`
+
+const Greetings = () => {
+    return (
+        <MainSection>
+            <h1>
+                <Ruby text='안녕하세요 😀' translation='Hello 😀' />
+            </h1>
+            <p>
+                <Ruby text='저를 소개합니다.' translation='Let me introduce myself.' />
+            </p>
+            <div>
+                <SlicedImage src={me} alt='me' />
+                <SlicedImage src='https://media.istockphoto.com/photos/cool-cow-picture-id139890530?k=6&m=139890530&s=612x612&w=0&h=Fz5NpzZnfXOjePbDTLIJWlewq9qFSfKVcVGbJGHdvnY=' alt='cow' />
+                <SlicedImage src='https://cdn.cashfeed.co.kr/attachments/94d04793e2.jpg' alt='dog' />
+            </div>
+        </MainSection>
+    );
+}
+
+export default Greetings;
